97dabe003d2db8662c2f439c4aab6e921919ab32
Akobir-1206 <akobiryuldashaliyev9@gmail.com>
2024-10-11 19:52:35 +0500

    addd

--- src/Component/index.jsx
+++ src/Component/index.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 export default function index() {
   return (
     <div>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, laudantium.</h1>
+      <h1>Akobir Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, laudantium.</h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam, in.</p>
-      
     </div>
   )
 }
