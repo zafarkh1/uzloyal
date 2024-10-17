652afd1e5f7837d402e419e25553dfcf82307280
Kamoliddin1701313 <kamol170professor@gmail.com>
2024-10-17 16:58:20 +0500

    Button componentdagi o'zgartirishlar

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -7,13 +7,12 @@ const Button = () => {
       <div
         className={`border-[1px] w-[20%] mx-auto text-center relative mt-8 h-[50px] border-solid border-[#cdb091]`}
       >
-       <button
-  type="submit"
-  className={`text-[#cdb091] links text-[14px] border-[1px] border-solid h-[50px] border-[#cdb091] w-full transform -translate-x-2 translate-y-[6px] relative overflow-hidden`}
->
-  Submit Now
-</button>
-
+        <button
+          type="submit"
+          className={`text-[#cdb091] links text-[14px] border-[1px] border-solid h-[50px] border-[#cdb091] w-full transform -translate-x-2 translate-y-[6px] relative overflow-hidden`}
+        >
+          Submit Now
+        </button>
       </div>
     </div>
   );
